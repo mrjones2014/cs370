20aa4095704916ffb6c9ba509f15799591518119
Mat Jones <mjones44@ycp.edu>
2017-09-07 16:28:56 -0400

    assign1

--- CS370_Assign01/CS370_Assign01/DonQuixote.cpp
+++ CS370_Assign01/CS370_Assign01/DonQuixote.cpp
@@ -12,57 +12,61 @@
 #define DEG2RAD 0.017453
 
 // Instance square - DO NOT MODIFY
-GLfloat sqr[4][2] = {{1.0f,1.0f},{-1.0f,1.0f},{-1.0f,-1.0f},{1.0f,-1.0f}};
+GLfloat sqr[4][2] = {
+	{1.0f,1.0f},{-1.0f,1.0f},
+	{-1.0f,-1.0f},{1.0f,-1.0f}
+};
 
 // Grass global variables
-GLfloat grass[4][2] = { {sqr[1][0], sqr[0][0] * 0}, {sqr[0][0], sqr[0][0] * 0}, {sqr[3][0], sqr[3][1]}, {sqr[2][0], sqr[2][1]} };
+GLfloat grass_square[4][2] = { {sqr[1][0], sqr[0][0] * 0}, {sqr[0][0], sqr[0][0] * 0}, {sqr[3][0], sqr[3][1]}, {sqr[2][0], sqr[2][1]} };
+
 // Sky global variables
-GLfloat sky[4][2];
+GLfloat sky_square[4][2] = {
+	{ 1.0f,1.0f },{ -1.0f,1.0f },
+	{ -1.0f,-1.0f },{ 1.0f,-1.0f }
+};
 
 // Sun global variables
 
-
 // House global variables
-GLfloat house[4][2] = { {.5*sqr[0][0], 0.10 * sqr[0][1]}, {.5 * sqr[2][0], 0.10 * sqr[0][0]}, {.5 * sqr[2][0] , .75 * sqr[1][0] }, {.5 * sqr[0][0], .75*sqr[1][0]} };
-GLfloat roof[3][2] = { {0 * sqr[0][0], .5 * sqr[0][1]}, { .5 * sqr[2][0], 0.1 * sqr[0][1]} , { .5*sqr[0][0], 0.1* sqr[0][1] } };
+GLfloat house_square[4][2] = { 
+	{.5 * sqr[0][0], 0.10 * sqr[0][1]},{.5 * sqr[2][0], 0.10 * sqr[0][0]},
+	{.5 * sqr[2][0] , .75 * sqr[1][0]},{ .5 * sqr[0][0], .75*sqr[1][0] }
+};
+GLfloat house_roof_triangle[3][2] = {
+	{0, 0.5*sqr[0][1]}, {0.5*sqr[2][0], 0.1*sqr[0][1]}, {0.5*sqr[0][0], 0.1*sqr[0][1]}
+};
+
 // Fan global variables
-GLfloat fan[6][2] = { {0,.5f}, {-.1f,.7f}, {0,.65}, { .1f,.7f }, { .1f,.65f } ,{ -.1f,.7f } };
-GLfloat fan1[6][2] = { {0,.5f },{-.1f,.3f },{ 0,.35 },{ .1f,.3f },{ .1f,.25f } ,{-.1f,.3f } };
-
-
+GLfloat theta = 0.0f;
+GLfloat fanSpeedIncrement = 0.0f;
+GLfloat fan_triangle[3][2] = {
+	{0, 0}, {-0.15*sqr[0][0], 0.3*sqr[1][1]}, {0.15*sqr[0][1], 0.3*sqr[1][1]}
+};
 
 void display();
 void render_Scene();
 void keyfunc(unsigned char key, int x, int y);
 void idlefunc();
 
+void drawSky();
+void drawGrass();
+void drawHouseBody();
+void drawHouseRoof();
+void drawHouse();
+void drawFanBlade(int _theta);
+
 int main(int argc, char* argv[])
 {
-	// Initialize GLUT
 	glutInit(&argc, argv);
-
-	// Initialize the window with double buffering and RGB colors
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-
-	// Set the window size to 500x500 pixels
 	glutInitWindowSize(500, 500);
-
-	// Create window
-	glutCreateWindow("Don Quixote!");
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 2; j++) {
-			sky[i][j] = sqr[i][j];
-		}
-	}
-	// Define callbacks
+	glutCreateWindow("don quixote");
 	glutDisplayFunc(display);
-
-	// Set background color to white
+	glutKeyboardFunc(keyfunc);
+	glutIdleFunc(idlefunc);
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-
-	// Begin event loop
 	glutMainLoop();
-
 	return 0;
 }
 
@@ -74,63 +78,91 @@ void display()
 
 	// Render scene
 	render_Scene();
-
+		
 	// Flush buffer
 	glFlush();
-
+	
 	// Swap buffers
 	glutSwapBuffers();
+
+	glutPostRedisplay();
 }
 
 // Scene render function
 void render_Scene()
 {
-	//render sky
-	glColor3f(0.0f, 0.0f, 1.0f);
+	drawSky();
+	drawGrass();
+	drawHouse();
+	drawFanBlade(0.0f + theta);
+	drawFanBlade(120.0f + theta);
+	drawFanBlade(240.0f + theta);
+}
+
+void drawSky() {
+	glColor3f(0.0f, 0.0f, 1.0f); //blue
 	glBegin(GL_POLYGON);
-	for (int i = 0; i < 4; i++) {
-		glVertex2fv(sky[i]);
+	for (int i = 0; i <4; i++) {
+		if (i == 2) {
+			glColor3f(1.0f, 1.0f, 1.0f); // back to white for gradient
+		}
+		glVertex2fv(sky_square[i]);
 	}
 	glEnd();
-	//render grass
-	glColor3f(0.0f, 1.0f, 0.0f);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+void drawGrass() {
+	glColor3f(0.0f, 1.0f, 0.0f); //green
 	glBegin(GL_POLYGON);
 	for (int i = 0; i < 4; i++) {
-		glVertex2fv(grass[i]);
+		glVertex2fv(grass_square[i]);
 	}
 	glEnd();
-	//build the house
-	glColor3f(0.545f, 0.27f, .074f);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+void drawHouse() {
+	drawHouseBody();
+	drawHouseRoof();
+}
+
+void drawHouseBody() {
+	glColor3f(0.5f, 0.25f, 0.08f); // brown ish
 	glBegin(GL_POLYGON);
 	for (int i = 0; i < 4; i++) {
-		glVertex2fv(house[i]);
+		glVertex2fv(house_square[i]);
 	}
 	glEnd();
-	glColor3f(1.0f, 0.0f, 0.0f);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+}
+
+void drawHouseRoof() {
+	glColor3f(1.0f, 0.0f, 0.0f); //red
 	glBegin(GL_POLYGON);
-	
 	for (int i = 0; i < 3; i++) {
-		glVertex2fv(roof[i]);
+		glVertex2fv(house_roof_triangle[i]);
 	}
-
-
-
-	//render the fans
-	glBegin(GL_TRIANGLE_FAN);
-	for (int i = 0; i < 4; i++) {
-		glColor3f(i/3, 1.0f, 1.0f);
-		glVertex2fv(fan[i]);
-	}
-
 	glEnd();
-	glBegin(GL_TRIANGLE_FAN);
-	for (int i = 0; i < 4; i++) {
-		glColor3f(i / 3, 1.0f, 1.0f);
-		glVertex2fv(fan1[i]);
-	}
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+}
 
+void drawFanBlade(int _theta) {
+	glPushMatrix();
+	glColor3f(0.0f, 1.0f, 1.0f); //cyan
+
+	GLfloat translateDist = house_roof_triangle[0][1] - fan_triangle[0][1];
+	glTranslatef(0.0f, translateDist, 0.0f);
+	glRotatef(_theta, 0.0f, 0.0f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < 3; i++) {
+		glVertex2fv(fan_triangle[i]);
+	}
 	glEnd();
-	glutSolidSphere(.15, 500, 20);
+
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glPopMatrix();
 }
 
 // Keyboard callback
@@ -141,10 +173,22 @@ void keyfunc(unsigned char key, int x, int y)
 	{
 		exit(0);
 	}
+	if (key == 32) {
+		fanSpeedIncrement += 10;
+	}
 }
 
 // Idle callback
 void idlefunc()
 {
+	if (fanSpeedIncrement != 0)
+	{
+		fanSpeedIncrement = fmax(fanSpeedIncrement - 0.05, 0);
+	}
 
+	if (fanSpeedIncrement != 0)
+	{
+		theta += fanSpeedIncrement;
+		glutPostRedisplay();
+	}
 }
